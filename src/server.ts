c918dd4345c053190b1cad2b0eab7af743326195
Sara Jegorova <saranordmyr@hotmail.com>
2021-06-11 19:42:32 +0300

    Fix wrong dist server path

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ import mongoose from "mongoose";
 import { Logger } from "./logger/logger";
 import app from "./app";
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 const isProdEnv = process.env.NODE_ENV === "production";
 
